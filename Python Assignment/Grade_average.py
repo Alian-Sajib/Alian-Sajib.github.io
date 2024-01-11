7f5454e4dd1732e5b3c4a9979bda89c95ab2ac59
Alian Sajib <aliansajib3@gmail.com>
2024-01-11 22:08:05 +0600

    update

--- Python Assignment/Grade_average.py
+++ Python Assignment/Grade_average.py
@@ -3,18 +3,20 @@ english = int(input("Enter the obtain marks of english : "))
 math = int(input("Enter the obtain marks of math : "))
 science = int(input("Enter the obtain marks of science : "))
 
-average = int((bangla+english+math+science)/4)
+average = int((bangla + english + math + science) / 4)
 print("Your Average marks is {}\n".format(average))
 
-if average>=91 and average<=100:
+if average >= 91 and average <= 100:
     print("You have got A+")
-elif average>=81 and average<=90:
+elif average >= 81 and average <= 90:
     print("You have got A")
-elif average>=71 and average<=80:
+elif average >= 71 and average <= 80:
     print("You have got B")
-elif average>=61 and average<=70:
+elif average >= 61 and average <= 70:
     print("You have got C")
-elif average>=41 and average<=60:
+elif average >= 41 and average <= 60:
     print("You have got D")
+elif average >= 0 and average <= 40:
+    print("You have got F")
 else:
-    print("You have got F ")
+    print("Error!.. Please insert correct marks ")
